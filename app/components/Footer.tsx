d1d0b60185a3c37ae35814cd7cc0cd0d564b85a6
ALmir Gomes da Silva Filho <almirgfill@gmail.com>
2025-07-22 16:52:58 -0300

    alterFooter

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-6 text-center text-sm text-gray-500 bg-gray-50">
-      © {new Date().getFullYear()} SeuNome. Todos os direitos reservados.
+      © {new Date().getFullYear()} R&B Soluctions. Todos os direitos reservados.
     </footer>
   )
 }
